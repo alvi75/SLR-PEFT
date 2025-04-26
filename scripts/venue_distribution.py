e4594ee6835c9f0a2487e764058ceda29fd4e2c4
Zahidul Haque Alvi <43238037+alvi75@users.noreply.github.com>
2025-04-26 12:08:34 -0400

    Update venue_distribution.py

--- scripts/venue_distribution.py
+++ scripts/venue_distribution.py
@@ -2,51 +2,39 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import MaxNLocator
 
-# Define the exact order and values
 venues = ['ACL', 'EMNLP', 'EMSE', 'FORGE', 'ICPC', 'ISSTA', 'KDD', 'MSR', 'NAACL', 'NeurIPS', 'SANER', 
  'arXiv', 'FSE', 'TSE', 'ICSE', 'TOSEM', 'ASE']
 paper_counts = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
 
-# Create a DataFrame
 df = pd.DataFrame({'Venue': venues, 'Number of Papers': paper_counts})
 
-# Reverse both the order of venues and corresponding colors
 df_reversed = df[::-1]
 colors = ['#E57373', '#4DB6AC', '#1976D2', '#FBC02D', '#C2185B', '#FF9800', '#BDBDBD', '#42A5F5', 
           '#FFAB91', '#81C784', '#1E88E5', '#F44336', '#B2EBF2', '#424242', '#9575CD', '#FFD600', 
           '#7E57C2', '#212121']
-colors_reversed = colors[::-1]  # Reverse the color combination
+colors_reversed = colors[::-1] 
 
-# Create the figure and axis
 fig, ax = plt.subplots(figsize=(7, 4))
 
-# Generate the bar plot
 ax.barh(df_reversed['Venue'], df_reversed['Number of Papers'], color=colors_reversed, edgecolor='black')
 
-# Adjust the x-axis limit to ensure ASE bar aligns with the X-axis
 ax.set_xlim(0, max(df_reversed["Number of Papers"]))
 
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
 
-# Labels and styling
 ax.set_xlabel("Number of Papers", fontsize=10)
-ax.set_ylabel("Venue")  # Remove "Venue" label
+ax.set_ylabel("Venue")
 ax.tick_params(axis='both', which='major', labelsize=10)
 
-# Match grid style
 ax.grid(axis='x', linestyle='--', alpha=0.7)
 ax.grid(axis='y', linestyle='--', alpha=0.7)
 
-# Ensure ASE fully overlaps with the X-axis
-ax.set_ylim(-0.5, len(df_reversed) - 0.5)  # Shifts all names down
+ax.set_ylim(-0.5, len(df_reversed) - 0.5) 
 
-# Remove the right and bottom spines
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 
-# Save the figure as a high-quality PNG file
 plt.savefig("venue_barplot.png", format="png", dpi=300, bbox_inches="tight")
 
-# Show the plot
 plt.show()
